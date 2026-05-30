b26904690ee1e24ba2a63c12dc76cf1660f60ec3
nnpuyen <nuyencrkh@gmail.com>
2026-05-30 14:16:48 +0700

    fix runtime error

--- trainer.py
+++ trainer.py
@@ -348,13 +348,14 @@ class Trainer:
                         self.task = task
                         self.batch_size = batch_size
                     
-                    def predict_by_examples(self, examples):
+                    def predict_by_examples(self, examples, batch_size=None):
                         self.model.eval()
                         with torch.no_grad():
+                            effective_batch_size = max(batch_size or self.batch_size, 512)
                             data_loader = torch.utils.data.DataLoader(
                                 Dataset(path='', examples=examples, task=self.task),
                                 num_workers=0,
-                                batch_size=max(self.batch_size, 512),
+                                batch_size=effective_batch_size,
                                 collate_fn=collate,
                                 shuffle=False)
                             hr_tensor_list, tail_tensor_list = [], []
