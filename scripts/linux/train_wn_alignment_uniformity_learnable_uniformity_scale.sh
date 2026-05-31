ce846b14a30824f8fb599c36459b27ecfdca6f6a
nnpuyen <nuyencrkh@gmail.com>
2026-05-31 14:52:48 +0700

    change log uniformity lr to 1e-4

--- scripts/linux/train_wn_alignment_uniformity_learnable_uniformity_scale.sh
+++ scripts/linux/train_wn_alignment_uniformity_learnable_uniformity_scale.sh
@@ -51,7 +51,7 @@ python3 -u main.py \
 --directau-eps 1e-12 \
 --directau-uniformity-scale 4 \
 --learnable-directau-uniformity-scale \
---log-uniformity-lr 5e-5 \
+--log-uniformity-lr 1e-4 \
 --no-negative-sampling \
 --epochs 50 \
 --workers 2 \
